f0ae0a9508c553874b5ca0633479b852468e9916
Phạm Văn Minh <phamvanminh150204@gmail.com>
2025-06-14 21:05:42 +0700

    add feature notification in doctor profile (#16)

--- src/components/doctor/DoctorProfile.jsx
+++ src/components/doctor/DoctorProfile.jsx
@@ -36,19 +36,6 @@ const mockDoctorData = {
   imageUrl: doctorProfileImage
 };
 
-// Data giả tạm thời
-const testData = {
-  id: 1,
-  name: 'Bs. minh',
-  specialty: 'Chuyên khoa HIV/AIDS',
-  email: 'doctor@fpt.edu.vn',
-  phoneNumber: '0987654399',
-  degree: 'Tiến sĩ Y khoa',
-  experience: 10,
-  certificates: ['Chứng chỉ hành nghề bác sĩ', 'Chuyên khoa HIV/AIDS'],
-  bio: 'Là bác sĩ với hơn 10 năm kinh nghiệm trong lĩnh vực điều trị HIV/AIDS. Chuyên môn sâu về quản lý và điều trị các bệnh liên quan đến HIV.',
-  imageUrl: doctorProfileImage
-};
 
 const DoctorProfile = () => {
   const [doctorData, setDoctorData] = useState(mockDoctorData);
