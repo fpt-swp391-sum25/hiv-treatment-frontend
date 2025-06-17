f9f6bcd5062b1752e2083ec9a285f593e2a3ad89
Phạm Văn Minh <phamvanminh150204@gmail.com>
2025-06-17 13:41:31 +0700

    fix UI doctor profile (#23)

--- src/components/doctor/App.jsx
+++ src/components/doctor/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import AppFooter from '../layouts/client/app-footer';
+import '../../styles/doctor/DoctorProfile.css';
 
 const DoctorApp = () => {
   return (
     <div className="doctor-app">
-      {/* Header có thể thêm sau */}
       <Container fluid className="py-4">
         <Outlet />
       </Container>
-      {<AppFooter />}
+      <AppFooter />
     </div>
   );
 };
