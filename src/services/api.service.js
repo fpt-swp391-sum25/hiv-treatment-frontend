4359e878e4186e7609bf5f1097ce872aebbf889d
Slayer <slayersilver2005@gmail.com>
2025-06-20 19:24:56 +0700

    fix test result time

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -188,9 +188,17 @@ const createTestResultAPI = (type, note, expectedResultTime, healthRecordId) => 
     return axios.post(URL_BACKEND, testResultData)
 }
 
-const updateTestResultAPI = (testResultId) => {
+const updateTestResultAPI = (testResultId, type, result, unit, note, expectedResultTime, actualResultTime) => {
+    const testResultData = {
+        type, 
+        result, 
+        unit, 
+        note, 
+        expectedResultTime, 
+        actualResultTime
+    }
     const URL_BACKEND = `api/test-result/${testResultId}`
-    return axios.put(URL_BACKEND)
+    return axios.put(URL_BACKEND, testResultData)
 }
 
 
